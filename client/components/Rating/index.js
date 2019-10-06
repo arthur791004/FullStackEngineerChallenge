341d3af1d5e989f6f3c48e9d85ad953a88df4c6e
arthur791004 <arthur791004@gmail.com>
2019-10-06 16:01:41 +0800

    add component: Rating

---
+++ client/components/Rating/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { number, func } from 'prop-types';
+import styled, { css } from 'styled-components';
+import { ReactComponent as StarBorderIcon } from '@/assets/svg/star_border.svg';
+import { ReactComponent as StarIcon } from '@/assets/svg/star.svg';
+import { Icon } from '@/components/Icons';
+
+const MAX_RATING = 5;
+
+const inactiveCSS = css`
+  svg {
+    &:last-child {
+      fill: none;
+    }
+  }
+`;
+
+const activeCSS = css`
+  svg {
+    &:last-child {
+      fill: #ffcb00;
+    }
+  }
+`;
+
+const Star = styled(Icon).attrs(({ size }) => ({
+  children: (
+    <>
+      <StarBorderIcon />
+      <StarIcon />
+    </>
+  ),
+  size,
+}))`
+  position: relative;
+  svg {
+    position: absolute;
+    fill: #ffcb00;
+  }
+
+  ${inactiveCSS};
+  ${props => props.isActive && activeCSS};
+`;
+
+const editableCSS = css`
+  &:hover {
+    ${activeCSS};
+  }
+
+  ${Star} {
+    cursor: pointer;
+  }
+
+  ${Star}:hover ~ ${Star} {
+    ${inactiveCSS};
+  }
+`;
+
+const Container = styled.div`
+  ${props => props.isEditable && editableCSS};
+`;
+
+const Rating = ({ rating, size, handleChange }) => (
+  <Container isEditable={!handleChange}>
+    {Array.from({ length: MAX_RATING }, (_, i) => (
+      <Star
+        key={i}
+        size={size}
+        isActive={i < rating}
+        onClick={() => handleChange(i)}
+      />
+    ))}
+  </Container>
+);
+
+Rating.propTypes = {
+  rating: number.isRequired,
+  size: number,
+  handleChange: func,
+};
+
+Rating.defaultProps = {
+  size: 24,
+  handleChange: null,
+};
+
+export default Rating;
